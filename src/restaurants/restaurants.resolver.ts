8340cf7ffe0a38dbf7a56a1233ec772806eb9921
texasroh <texasroh@gmail.com>
2023-01-01 21:19:00 -0500

    add class validator

---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,15 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateRestaurantDto } from './dto/create-restaurant.dto';
+import { Restaurant } from './entities/restaurant.entity';
+
+@Resolver((of) => Restaurant)
+export class RestaurantResolver {
+  @Query((returns) => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
+  }
+  @Mutation((returns) => Boolean)
+  createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
+    return true;
+  }
+}
